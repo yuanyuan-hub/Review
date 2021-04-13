d89e86b913459b725d2f6b1c58c36ebb8db58499
yuanyuan-hub <68865212+yuanyuan-hub@users.noreply.github.com>
2021-04-13 20:08:12 +0800

    多线程1.1

--- 20210412/src/lesson1/ThreadLook4.java
+++ 20210412/src/lesson1/ThreadLook4.java
@@ -1,4 +1,20 @@
 package lesson1;
 
 public class ThreadLook4 {
+    public static void main(String[] args) {
+        //同时启动20个线程，每个线程从0，+1的方式加到9999
+        for (int i = 0; i < 20; i++) {
+            Thread t = new Thread(){
+                @Override
+                public void run() {
+                    for (int j = 0; j < 10000; j++) {
+                        if(j == 9999) {
+                            System.out.println(i);
+                        }
+                    }
+                }
+            };
+            t.start();
+        }
+    }
 }
