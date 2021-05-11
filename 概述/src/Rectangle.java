76e93011770d38d1dc745c2d8bdc7acdf875e544
yuanyuan-hub <68865212+yuanyuan-hub@users.noreply.github.com>
2021-05-11 23:52:15 +0800

    Rectangle的矩形类。

--- 概述/src/Rectangle.java
+++ 概述/src/Rectangle.java
@@ -1,2 +1,34 @@
 public class Rectangle {
+    private float width,height;
+    private String color;
+    public Rectangle(float width, float height, String color) {
+        this.width = width;
+        this.height = height;
+        this.color = color;
+    }
+    public float getWidth() {
+        return width;
+    }
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public  float getArea(float width, float height) {
+        float area = 0;
+        area = width*height;
+        return area;
+    }
+    public  float getLength(float width, float height) {
+        float length = 0;
+        length = 2 *(width+height);
+        return length;
+    }
+
+    public static void main(String[] args) {
+        Rectangle rectangle = new Rectangle(5,8,"red");
+        System.out.println(rectangle.getWidth());
+        rectangle.setColor("blue");
+        System.out.println(rectangle.getArea(5,4));
+        System.out.println(rectangle.getLength(2,6));
+    }
 }
